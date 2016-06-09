010ade5a5af4e409b4c47817af48e1bd9e357d8a
Drew Gorton <dgorton@users.noreply.github.com>
2016-06-08 21:55:14 -0500

    DISALLOW_FILE_MODS is already defined
    Removing duplicative lines

--- wordpress/secure-updates.wp-config.php
+++ wordpress/secure-updates.wp-config.php
@@ -1,10 +1,4 @@
-/** Restrict Auto Updating features in admin */
+/** Never edit PHP via the web. Even in Dev */
 if (defined('PANTHEON_ENVIRONMENT')) {
-  // Never edit PHP via the web. Even in Dev
   define( 'DISALLOW_FILE_EDIT', true );
-  // Background updates must be done ony in Dev
-  if (!in_array(PANTHEON_ENVIRONMENT, array('test', 'live'))) {
-		define( 'AUTOMATIC_UPDATER_DISABLED', true );
-    define( 'WP_AUTO_UPDATE_CORE', false );
-  }
 }
