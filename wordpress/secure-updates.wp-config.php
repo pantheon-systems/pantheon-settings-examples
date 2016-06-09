f4e40acde2903a5405efeaa234fd9aef6242a2f4
Drew Gorton <dgorton@users.noreply.github.com>
2016-06-08 22:27:32 -0500

    Code styling and error checking
    Less late night coding would be better.

--- wordpress/secure-updates.wp-config.php
+++ wordpress/secure-updates.wp-config.php
@@ -1,4 +1,4 @@
 /** Never edit PHP via the web. Even in Dev */
-if (defined('PANTHEON_ENVIRONMENT')) {
+if (defined( 'PANTHEON_ENVIRONMENT' ) && !defined( 'DISALLOW_FILE_EDIT' )) {
   define( 'DISALLOW_FILE_EDIT', true );
 }
